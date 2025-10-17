c78b8e93ae120640e54ec4ef1936ae22566592fb
s2selyn <s2selyn@gmail.com>
2025-10-17 16:51:19 +0900

    MemberController.java

---
+++ SimpleProject/src/main/java/com/kh/spring/member/controller/MemberController.java
@@ -0,0 +1,13 @@
+package com.kh.spring.member.controller;
+
+public class MemberController {
+	
+	// 서블릿을 더이상 만들지 않지만, 서블릿이 사라진것은 아님
+	// 서버 구동하면 web.xml을 읽는다
+	// console에 서버 구동하면 보이는 DispatcherServlet <- 얘가 본부장, 커맨드센터, 진두지휘하는녀석
+	// 이건 어디서 튀어나왔나? web.xml을 읽는다 -> 가서 보면됨
+	// listener를 올림
+	// processes application requests 주석 아래에 servlet 태그가 있음
+	// 이거보면 ... 어쩌고 있으니 풀클래스명이니 이게 클래스
+
+}
